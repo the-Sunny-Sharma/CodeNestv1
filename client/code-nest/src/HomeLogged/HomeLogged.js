6868e2bde53d7716a7510ab0c06897fab1a4e642
Sunny Sharma <syn8433898723@gmail.com>
2024-03-26 19:11:52 +0530

    added search icon in Courses Page

--- client/code-nest/src/HomeLogged/HomeLogged.js
+++ client/code-nest/src/HomeLogged/HomeLogged.js
@@ -7,40 +7,27 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function HomeLogged() {
   const navigate = useNavigate();
-  const [userInfo, setUserInfo] = useState(null);
 
-  useEffect(() => {
-    const userInfo = localStorage.getItem("user");
-    if (!userInfo) {
-      const url = "http://localhost:4000/api/v1/logout";
-      localStorage.clear();
-      try {
-        const response = axios.get(url, {
-          withCredentials: true,
-        });
-        if (response.data.success === true) {
-          navigate("/");
-        }
-      } catch (error) {
-        toast.error("Something went wrong!");
-      }
-    }
-  });
+  // useEffect(() => {
+  //   const userInfo = localStorage.getItem("user");
+  //   if (!userInfo) {
+  //     const url = "http://localhost:4000/api/v1/logout";
+  //     localStorage.clear();
+  //     try {
+  //       const response = axios.get(url, {
+  //         withCredentials: true,
+  //       });
+  //       if (response.data.success === true) {
+  //         navigate("/");
+  //       }
+  //     } catch (error) {
+  //       toast.error("Something went wrong!");
+  //     }
+  //   }
+  // });
 
   return (
     <>
-      {/* <h1>Welcome</h1>
-      {userInfo ? (
-        <div>
-          <p>
-            Name: {userInfo.fName} {userInfo.lName}
-          </p>
-          <p>Email: {userInfo.email}</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <p>Loading user profile...</p>
-      )} */}
       <Caurosel />
       <ToastContainer position="bottom-right" autoClose={3000} />
     </>
